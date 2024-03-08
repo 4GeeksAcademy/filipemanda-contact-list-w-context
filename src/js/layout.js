ec697ee27e2595ce0d368f45eb1bcb666f827c2a
filipemanda <134346665+filipemanda@users.noreply.github.com>
2024-03-08 17:54:44 +0000

    notdone

--- src/js/layout.js
+++ src/js/layout.js
@@ -1,35 +1,52 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import ScrollToTop from "./component/scrollToTop";
 
-import { Home } from "./views/home";
+
 import { Demo } from "./views/demo";
 import { Single } from "./views/single";
 import injectContext from "./store/appContext";
 
 import { Navbar } from "./component/navbar";
 import { Footer } from "./component/footer";
+import { Contacts } from "./views/Contacts";
+import { Modal } from "./component/Modal";
+import { AddContact } from "./views/AddContact";
+import { ContactCard } from "./component/ContactCard";
 
+export const AppContext = React.createContext();
 //create your first component
 const Layout = () => {
 	//the basename is used when your project is published in a subdirectory and not in the root of the domain
 	// you can set the basename on the .env file located at the root of this project, E.g: BASENAME=/react-hello-webapp/
 	const basename = process.env.BASENAME || "";
+	const [contacts, setcontacts] = useState([]);
+	useEffect(() => {
+		fetch("https://playground.4geeks.com/apis/fake/contact/agenda/filipemanda")
+			.then((resp) => resp.json())
+			.then((data) => (setcontacts(data)));
+	}, []);
 
 	return (
 		<div>
-			<BrowserRouter basename={basename}>
-				<ScrollToTop>
-					<Navbar />
-					<Routes>
-						<Route path="/" element={<Home />} />
-						<Route path="/demo" element={<Demo />} />
-						<Route path="/single/:theid" element={<Single />} />
-						<Route path="*" element={<h1>Not found!</h1>} />
-					</Routes>
-					<Footer />
-				</ScrollToTop>
-			</BrowserRouter>
+			<AppContext.Provider value={{ contacts, setcontacts }}>
+				<BrowserRouter basename={basename}>
+					<ScrollToTop>
+						<Navbar />
+						<Routes>
+
+							<Route path="/" element={<Contacts />} />
+							<Route path="/modal" element={<Modal />} />
+							<Route path="/addcontact" element={<AddContact />} />
+							<Route path="/contactcard" element={<ContactCard />} />
+							<Route path="/demo" element={<Demo />} />
+							<Route path="/single/:theid" element={<Single />} />
+							<Route path="*" element={<h1>Not found!</h1>} />
+						</Routes>
+						<Footer />
+					</ScrollToTop>
+				</BrowserRouter>
+			</AppContext.Provider>
 		</div>
 	);
 };
